c4e5655fe946e5d681d3a563ed5f4834762754d5
Stacy Vlasits <stacyvlasits@gmail.com>
2012-05-16 13:24:20 -0400

    added skills methods to Character

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,17 +9,16 @@ class Character < ActiveRecord::Base
   validates_numericality_of :str, :int, :wis, :dex, :con, :chr, 
     :greater_than_or_equal_to => 1, :only_integer => true
 
-  skills = ["hp", "bab", "ac"]
+  stats = ["hp", "bab", "ac"]
+  Skills = { :"Appraise" => 'Int', :"Autohypnosis" => 'Wis', :"Balance" => 'Dex', :"Bluff" => 'Cha', :"Climb" => 'Str', :"Concentration" => 'Con', :"Control Shape" => 'Wis', :"Craft" => 'Int', :"Decipher Script" => 'Int', :"Diplomacy" => 'Cha', :"Disable Device" => 'Int', :"Disguise" => 'Cha', :"Escape Artist" => 'Dex', :"Forgery" => 'Int', :"Gather Information" => 'Cha', :"Handle Animal" => 'Cha', :"Heal" => 'Wis', :"Hide" => 'Dex', :"Intimidate" => 'Cha', :"Jump" => 'Str', :"Knowledge" => 'Int', :"Listen" => 'Wis', :"Move Silently" => 'Dex', :"Open Lock" => 'Dex', :"Perform" => 'Cha', :"Psicraft" => 'Int', :"Profession" => 'Wis', :"Ride" => 'Dex', :"Search" => 'Int', :"Sense Motive" => 'Wis', :"Sleight of Hand" => 'Dex', :"Speak Language" => 'none', :"Spellcraft" => 'Int', :"Spot" => 'Wis', :"Survival" => 'Wis', :"Swim" => 'Str', :"Tumble" => 'Dex', :"Use Magic Device" => 'Cha', :"Use Psionic Device" => 'Cha', :"Use Rope" => 'Dex'}
 
+  skills = Skills.keys
 
-  skills.each do |method_name|
-    define_method method_name do 
+  (skills + stats).each do |method_name|
+     method_name = method_name.to_s.downcase.gsub(/\s/,'_')
+     define_method method_name do 
       self.modifiers.where(:kind => method_name).sum(:amount)  
     end    
   end
   
-  # def hp
-  #   self.modifiers.current("hp").sum(:amount)
-  # end
-
 end
