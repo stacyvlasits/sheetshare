fb1eacab4097e837e3e1e8b2f05d18c74e8f8902
Stacy Vlasits <stacyvlasits@gmail.com>
2012-05-16 16:57:05 -0400

    cleaned up the views, added an owner method to Gears

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,13 +9,15 @@ class Character < ActiveRecord::Base
   validates_numericality_of :str, :int, :wis, :dex, :con, :chr, 
     :greater_than_or_equal_to => 1, :only_integer => true
 
-  stats = ["hp", "bab", "ac"]
+  Stats = ["hp", "bab", "ac"]
   Skills = { :"Appraise" => 'Int', :"Autohypnosis" => 'Wis', :"Balance" => 'Dex', :"Bluff" => 'Cha', :"Climb" => 'Str', :"Concentration" => 'Con', :"Control Shape" => 'Wis', :"Craft" => 'Int', :"Decipher Script" => 'Int', :"Diplomacy" => 'Cha', :"Disable Device" => 'Int', :"Disguise" => 'Cha', :"Escape Artist" => 'Dex', :"Forgery" => 'Int', :"Gather Information" => 'Cha', :"Handle Animal" => 'Cha', :"Heal" => 'Wis', :"Hide" => 'Dex', :"Intimidate" => 'Cha', :"Jump" => 'Str', :"Knowledge" => 'Int', :"Listen" => 'Wis', :"Move Silently" => 'Dex', :"Open Lock" => 'Dex', :"Perform" => 'Cha', :"Psicraft" => 'Int', :"Profession" => 'Wis', :"Ride" => 'Dex', :"Search" => 'Int', :"Sense Motive" => 'Wis', :"Sleight of Hand" => 'Dex', :"Speak Language" => 'none', :"Spellcraft" => 'Int', :"Spot" => 'Wis', :"Survival" => 'Wis', :"Swim" => 'Str', :"Tumble" => 'Dex', :"Use Magic Device" => 'Cha', :"Use Psionic Device" => 'Cha', :"Use Rope" => 'Dex'}
 
   skills = Skills.keys
+  stats = Stats
 
+  # TODO:  re-write this so the values are cached and reused.  Too many queries on the database here.
   (skills + stats).each do |method_name|
-     method_name = method_name.to_s.downcase.gsub(/\s/,'_')
+    method_name = method_name.to_s.downcase.gsub(/\s/,'_')
      define_method method_name do 
       self.modifiers.where(:kind => method_name).sum(:amount)  
     end    
--- app/models/gear.rb
+++ app/models/gear.rb
@@ -4,4 +4,13 @@ class Gear < ActiveRecord::Base
 
   validates :name, :slot, :presence => true
   
+  def stats
+    stats = []
+    self.modifiers.group_by(&:kind).each {|kind,other| stats << "#{kind}:  #{other.sum(&:amount)}"}
+    stats
+  end
+  def owner
+    Character.find(self.character_id).name
+  end
+
 end
